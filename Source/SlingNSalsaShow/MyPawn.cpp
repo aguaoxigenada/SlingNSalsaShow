8c7a8055aca2dab1984ced22d3756c0c2b0e3810
Joan Odicio <jodicio@betomorrow.com>
2024-01-27 19:53:49 +0100

    Adding MyPawn controller

---
+++ Source/SlingNSalsaShow/MyPawn.cpp
@@ -0,0 +1,117 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyPawn.h"
+#include "Components/BoxComponent.h"
+
+// Sets default values
+AMyPawn::AMyPawn()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	// Create a root scene component
+	USceneComponent* RootSceneComponent = CreateDefaultSubobject<USceneComponent>(TEXT("RootSceneComponent"));
+	RootComponent = RootSceneComponent;
+	
+	// Init BoxComponent
+	UBoxComponent* BoxCollision  = CreateDefaultSubobject<UBoxComponent>(TEXT("BoxComponent"));
+	
+	// Init Mesh
+	myMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MyMesh"));
+	myMesh->SetupAttachment(RootSceneComponent);
+
+	// Init BoxComponent
+	myBoxCollision = CreateDefaultSubobject<UBoxComponent>(TEXT("MyBoxComponent"));
+	myBoxCollision->SetupAttachment(myMesh);
+	myBoxCollision->SetCollisionProfileName(TEXT("OverlapAll"));
+
+	myMesh->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+	myMesh->SetCollisionResponseToChannel(ECC_Visibility, ECR_Overlap);
+
+	myMesh->OnClicked.AddDynamic(this, &AMyPawn::OnClicked);
+	GetController()->OnReleased.AddDynamic(this, &AMyPawn::OnReleased);
+}
+
+// Called when the game starts or when spawned
+void AMyPawn::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AMyPawn::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AMyPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
+void AMyPawn::OnClicked(UPrimitiveComponent* UPrimitiveComponent, FKey ButtonPressed)
+{
+	GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, TEXT("OnClicked"));
+	UE_LOG(LogTemp, Display, TEXT("OnClicked"));
+}
+
+void AMyPawn::OnReleased(AActor* UPrimitiveComponent, FKey ButtonReleased)
+{
+	GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, TEXT("OnReleased"));
+
+	FVector playerLocation =  GetActorLocation();
+	EPairedAxis pairedAxis = ButtonReleased.GetPairedAxis();
+	
+	UE_LOG(LogTemp, Display, TEXT("Vector value: %s"), *playerLocation.ToString());
+	
+	const APlayerController* PlayerController = GetWorld()->GetFirstPlayerController();
+	if(PlayerController != nullptr)
+	{
+		float MouseX, MouseY;
+		if (PlayerController->GetMousePosition(MouseX, MouseY))
+		{
+			//MouseX and MouseY now contain the position of the mouse
+
+			FVector WorldLocation, WorldDirection;
+			if (PlayerController->DeprojectScreenPositionToWorld(MouseX, MouseY, WorldLocation, WorldDirection))
+			{
+				// Raycast or use the direction to determine the world position
+				FVector EndPoint = WorldLocation + (WorldDirection * 10000); // Arbitrary distance
+				FHitResult HitResult;
+				if (GetWorld()->LineTraceSingleByChannel(HitResult, WorldLocation, EndPoint, ECC_Visibility))
+				{
+					FVector MouseWorldPosition = HitResult.Location;
+					// MouseWorldPosition is the location in the world where the mouse is pointing
+					UE_LOG(LogTemp, Display, TEXT("Mouse position value: %s"), *MouseWorldPosition.ToString());
+
+					if(Plane.Get() != nullptr)
+					{
+						FVector planePoint = FVector(0, 0, -50);
+						FVector planeUpVector = Plane.Get()->GetActorUpVector(); 
+						FVector projectedActorPoint = FVector::PointPlaneProject(playerLocation, planePoint, planeUpVector).GetSafeNormal();
+						FVector projectedMousePoint = FVector::PointPlaneProject(MouseWorldPosition, planePoint, planeUpVector).GetSafeNormal();
+
+						// void DrawDebugDirectionalArrow
+						// (
+						//     const UWorld * InWorld,
+						//     FVector const & LineStart,
+						//     FVector const & LineEnd,
+						//     float ArrowSize,
+						//     FColor const & Color,
+						//     bool bPersistentLines,
+						//     float LifeTime,
+						//     uint8 DepthPriority,
+						//     float Thickness
+						// )
+						// UE_LOG(LogTemp, Warning, TEXT("=======> DRAWING ARROW"));
+						// DrawDebugDirectionalArrow(GetWorld(), playerLocation, MouseWorldPosition, 300.f, FColor::Red, true, 0.f, 5, 2.f);
+					}
+				}	
+			}
+		}	
+	}
+}
+
---
+++ Source/SlingNSalsaShow/MyPawn.h
@@ -0,0 +1,47 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/BoxComponent.h"
+#include "GameFramework/Pawn.h"
+#include "MyPawn.generated.h"
+
+UCLASS()
+class SLINGNSALSASHOW_API AMyPawn : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	AMyPawn();
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	UBoxComponent* myBoxCollision = nullptr;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	UStaticMeshComponent* myMesh = nullptr;
+
+	// Handler for when the actor is clicked
+	UFUNCTION()
+	void OnClicked(UPrimitiveComponent* TouchedActor, FKey ButtonPressed);
+
+	// Handler for when the click is released
+	UFUNCTION()
+	void OnReleased(AActor* TouchedActor, FKey ButtonReleased);
+	
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+private:
+	UPROPERTY(EditInstanceOnly)
+	TWeakObjectPtr<AActor> Plane;
+};
